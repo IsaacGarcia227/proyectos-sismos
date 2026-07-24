b927beecb603ac0a2db864f13a3dcd2b37c6e5a4
Isaac Garcia <al22760532@ite.edu.mx>
2026-07-23 22:56:43 -0700

    Agrego ejercicio de arreglos en c

---
+++ semana1/ejercicios/arreglos.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+	float onda[5] = {0.0, 1.5, 3.2, 2.1, 0.8};
+	int i;
+
+	printf("Amplitudes de onda sismica:/n");
+	for (i = 0; i<5; i++) {
+		printf("Punto %d: %.1f/n", i, onda[i]);
+	}
+
+	return 0;
+
+}
