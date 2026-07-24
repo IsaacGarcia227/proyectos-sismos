c1c2670a43b4d16f33594db4728720c29a84ef54
Isaac Garcia <al22760532@ite.edu.mx>
2026-07-23 23:03:58 -0700

    Corrijo arreglos.c - diagonal y corchetes

--- semana1/ejercicios/arreglos.c
+++ semana1/ejercicios/arreglos.c
@@ -5,9 +5,9 @@ int main() {
 	float onda[5] = {0.0, 1.5, 3.2, 2.1, 0.8};
 	int i;
 
-	printf("Amplitudes de onda sismica:/n");
+	printf("Amplitudes de onda sismica:\n");
 	for (i = 0; i<5; i++) {
-		printf("Punto %d: %.1f/n", i, onda[i]);
+		printf("Punto %d: %.1f\n", i, onda[i]);
 	}
 
 	return 0;
